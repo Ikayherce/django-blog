b86ab1dcbd0bda3020ed80d93d620db48a2a2245
Ika Yébenes Herce <ikayherce@gmail.com>
2024-03-19 10:21:45 +0000

    migrations corrected

--- blog/models.py
+++ blog/models.py
@@ -23,15 +23,6 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
-class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
-    body = models.TextField()
-    approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)
-
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -41,3 +32,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
